0a13c3a114e04252dd4e9aad76f70ea6298a2c24
ruchi-115 <71343258+ruchi-115@users.noreply.github.com>
2021-06-10 21:24:00 +0530

    Add files via upload

---
+++ ruchi-115-cs50-My Final Project/README.md
@@ -0,0 +1,26 @@
+# F!T 50
+
+### Environment :HTML, Javascript, Python, Flask, SQL, Jinja, CSS.
+
+##About
+For my CS50 Final Project I made something that could be useful to me and others.
+It is a simple fitness web application which includes workout page, health amd food page.
+Fit 50 provides you with interactive resources for keeping yourself fit and healthy.
+
+To run it: flask run
+
+## Features
+- Application has login and register id which is stored in a sql database.
+- Home is link to different pages and various stylesheets are used.
+- Videos from youttube are embed in the website.
+- Gifs are used to make the exercises interactive and fun.
+- Javascript is used to make pages interactive and functional.
+
+## Resources
+  Most of the information is obtained by [W3School](https://www.w3schools.com/)
+  Also used
+  [giphy](https://giphy.com/)
+  exercises information from[active times](https://www.theactivetimes.com/)
+  images from [unsplash](https://unsplash.com/s/photos/fitness)
+
+
---
+++ ruchi-115-cs50-My Final Project/application.py
@@ -0,0 +1,200 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure responses aren't cached
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///fit50.db")
+
+
+@app.route("/")
+@login_required
+def index():
+
+    # look up the current user
+    users = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])
+    return render_template("home.html")
+
+@app.route("/health", methods=["GET", "POST"])
+def health():
+    return render_template("health.html")
+
+
+
+@app.route("/workout", methods=["GET", "POST"])
+@login_required
+def workout():
+    return render_template("workout.html")
+
+@app.route("/change_password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    """Allow user to change her password"""
+
+    if request.method == "POST":
+
+        # Ensure current password is not empty
+        if not request.form.get("current_password"):
+            return apology("must provide current password", 400)
+
+        # Query database for user_id
+        rows = db.execute("SELECT hash FROM users WHERE id = :user_id", user_id=session["user_id"])
+
+        # Ensure current password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("current_password")):
+            return apology("invalid password", 400)
+
+        # Ensure new password is not empty
+        if not request.form.get("new_password"):
+            return apology("must provide new password", 400)
+
+        # Ensure new password confirmation is not empty
+        elif not request.form.get("new_password_confirmation"):
+            return apology("must provide new password confirmation", 400)
+
+        # Ensure new password and confirmation match
+        elif request.form.get("new_password") != request.form.get("new_password_confirmation"):
+            return apology("new password and confirmation must match", 400)
+
+        # Update database
+        hash = generate_password_hash(request.form.get("new_password"))
+        rows = db.execute("UPDATE users SET hash = :hash WHERE id = :user_id", user_id=session["user_id"], hash=hash)
+
+        # Show flash
+        flash("Changed!")
+
+    return render_template("change_password.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect(url_for("index"))
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect(url_for("index"))
+
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Ensure password and confirmation match
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+
+        # hash the password and insert a new user in the database
+        hash = generate_password_hash(request.form.get("password"))
+        new_user_id = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
+                                 username=request.form.get("username"),
+                                 hash=hash)
+
+        # unique username constraint violated?
+        if not new_user_id:
+            return apology("username taken", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = new_user_id
+
+        # Display a flash message
+        flash("Registered!")
+
+        # Redirect user to home page
+        return redirect(url_for("index"))
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+
+def errorhandler(e):
+    """Handle error"""
+    return apology(e.name, e.code)
+
+
+# listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
